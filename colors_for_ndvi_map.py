61bd61ff5e2dee892bc18af549213204ea020194
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2020-09-14 20:33:32 +0600

    Add new color map

--- colors_for_ndvi_map.py
+++ colors_for_ndvi_map.py
@@ -4,11 +4,11 @@ from PyQt4.QtGui import QColor
 class ColorsForNdviMap:
     colorSchemes = {
         "1": {
-            "ndvi_0": QColor(4, 18, 60, 255),
-            "ndvi_0.25": QColor(148, 114, 60, 255),
-            "ndvi_0.5": QColor(148, 182, 20, 255),
-            "ndvi_0.75": QColor(60, 134, 4, 255),
-            "ndvi_1": QColor(4, 38, 4, 255)
+            "ndvi_0": QColor(165, 0, 38, 255),
+            "ndvi_0.25": QColor(247, 134, 78, 255),
+            "ndvi_0.5": QColor(254, 247, 180, 255),
+            "ndvi_0.75": QColor(157, 213, 105, 255),
+            "ndvi_1": QColor(12, 127, 67, 255)
         },
         "2": {
             "ndvi_0": QColor(99, 0, 1, 255),
