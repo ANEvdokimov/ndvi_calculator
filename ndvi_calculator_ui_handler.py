605d350785793b2014ab308dc0551aed75286a23
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2020-10-29 15:22:53 +0600

    Fix missing icon

--- ndvi_calculator_ui_handler.py
+++ ndvi_calculator_ui_handler.py
@@ -181,7 +181,7 @@ class ndvi_calculator_ui_handler(QObject):
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/ndvi_calculator/icon.png'
+        icon_path = os.path.join(self.plugin_dir, 'icon.png')
         self.add_action(
             icon_path,
             text=self.getTranslation(u'Calculate NDVI'),
