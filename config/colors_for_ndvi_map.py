b25dcb5c339be7b5c30849e07f0df8823291666d
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2020-11-24 21:18:32 +0600

    add and rename colors

--- config/colors_for_ndvi_map.py
+++ config/colors_for_ndvi_map.py
@@ -3,26 +3,33 @@ from PyQt4.QtGui import QColor
 
 class ColorsForNdviMap:
     colorSchemes = {
-        "1": {
-            "ndvi_0": QColor(165, 0, 38, 255),
-            "ndvi_0.25": QColor(247, 134, 78, 255),
-            "ndvi_0.5": QColor(254, 247, 180, 255),
-            "ndvi_0.75": QColor(157, 213, 105, 255),
-            "ndvi_1": QColor(12, 127, 67, 255)
-        },
-        "2": {
+        "1.RdGr": {
             "ndvi_0": QColor(99, 0, 1, 255),
             "ndvi_0.25": QColor(237, 118, 0, 255),
             "ndvi_0.5": QColor(255, 226, 0, 255),
             "ndvi_0.75": QColor(0, 211, 10, 255),
             "ndvi_1": QColor(4, 38, 4, 255)
         },
-        "3": {
+        "2.BlGr": {
+            "ndvi_0": QColor(44, 10, 121, 255),
+            "ndvi_0.25": QColor(245, 10, 0, 255),
+            "ndvi_0.5": QColor(250, 125, 0, 255),
+            "ndvi_0.75": QColor(246, 253, 3, 255),
+            "ndvi_1": QColor(31, 119, 0, 255)
+        },
+        "3.BlRd": {
             "ndvi_0": QColor(0, 0, 99, 255),
             "ndvi_0.25": QColor(0, 62, 255, 255),
             "ndvi_0.5": QColor(0, 255, 227, 255),
             "ndvi_0.75": QColor(239, 249, 0, 255),
             "ndvi_1": QColor(250, 103, 44, 255)
+        },
+        "4.Spectral": {
+            "ndvi_0": QColor(215, 25, 28, 255),
+            "ndvi_0.25": QColor(253, 174, 97, 255),
+            "ndvi_0.5": QColor(255, 255, 191, 255),
+            "ndvi_0.75": QColor(171, 221, 164, 255),
+            "ndvi_1": QColor(43, 131, 186, 255)
         }
     }
 
