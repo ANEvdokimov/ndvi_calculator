792dc0921d9d7e71298269b21c1df0f760c4e0e6
Alexey Evdokimov <alexey.nikolaevich.e@gmail.com>
2020-11-26 15:14:19 +0600

    add English UI has been translated into English and a Russian translation file has been created.

---
+++ calc.pro
@@ -0,0 +1,4 @@
+FORMS = ./v_calculator_dialog_base.ui
+SOURCES = ./v_calculator_main.py
+TRANSLATIONS += i18n/vegetation_calculator_ru.ts
+CODECFORSRC = UTF-8
---
+++ i18n/vegetation_calculator_ru.ts
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>ndvi_calculatorDialogBase</name>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="39"/>
+        <source>NDVI</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="789"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="48"/>
+        <source>Calculate NDVI</source>
+        <translation>Рассчитать NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="532"/>
+        <source>Agriculture and healthy vegetation</source>
+        <translation>Агрокультура и здоровая растительность</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="577"/>
+        <source>Raster bands</source>
+        <translation>Каналы растров</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="138"/>
+        <source>Red</source>
+        <translation>Красный</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="643"/>
+        <source>Infrared</source>
+        <translation>Инфракрасный</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="285"/>
+        <source>Output layer</source>
+        <translation type="obsolete">Слой результатов</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="330"/>
+        <source>Open file with calculated NDVI</source>
+        <translation>Открыть файл с рассчитанным NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="397"/>
+        <source>File with NDVI</source>
+        <translation>Файл с NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="496"/>
+        <source>Color scheme</source>
+        <translation>Цветовая схема</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="760"/>
+        <source>Output file</source>
+        <translation>Файл результатов</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="543"/>
+        <source>Agriculture</source>
+        <translation>Агрокультура</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="556"/>
+        <source>Healthy vegetation</source>
+        <translation>Здоровая растительность</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="603"/>
+        <source>Shortwave infrared</source>
+        <translation>Коротковолновый ИК</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="683"/>
+        <source>Blue</source>
+        <translation>Синий</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="45"/>
+        <source>Calculate NDVI based on a photo from a UAV or spacecraft</source>
+        <translation>Рассчитать NDVI на основе фотографии с БЛА или космического аппарата</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="574"/>
+        <source>Bands of bitmap image</source>
+        <translation>Каналы растрового изображения</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="135"/>
+        <source>Red band for NDVI calculation</source>
+        <translation>Красный канал для расчета NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="154"/>
+        <source>Image containing a red band</source>
+        <translation>Изображение, содержащее красный канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="170"/>
+        <source>Red band</source>
+        <translation>Красный канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="193"/>
+        <source>Infrared band for NDVI calculation</source>
+        <translation>Инфракрасный канал для расчета NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="650"/>
+        <source>Image containing a infrared band</source>
+        <translation>Изображение, содержащее инфракрасный канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="657"/>
+        <source>Infrared band</source>
+        <translation>Инфракрасный канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="767"/>
+        <source>The file where the result will be saved</source>
+        <translation>Файл, куда будет сохранен результат</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="786"/>
+        <source>File selection dialog</source>
+        <translation>Диалог выбора файла</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="327"/>
+        <source>Open single band image with calculated NDVI</source>
+        <translation>Открыть одноканальное изображение с рассчитанным NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="404"/>
+        <source>File with calculated NDVI</source>
+        <translation>Файл с рассчитанным NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="515"/>
+        <source>Colors indicating different NDVI values</source>
+        <translation>Цвета, обозначающие разные значения NDVI</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="20"/>
+        <source>Vegetation ?alculator</source>
+        <translation type="obsolete">Вегетационный калькулятор</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="540"/>
+        <source>Agriculture Index</source>
+        <translation>Индекс &quot;Агрокультура&quot;</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="553"/>
+        <source>Healthy Vegetation Index</source>
+        <translation>Индекс &quot;Здоровая растительность&quot;</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="617"/>
+        <source>Shortwave infrared band</source>
+        <translation>Коротковолновый инфракрасный канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="610"/>
+        <source>Image containing a SWIR band</source>
+        <translation>Изображение, содержащее коротковолновый инфракрасный канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="697"/>
+        <source>Blue band</source>
+        <translation>Синий канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="690"/>
+        <source>Image containing a blue band</source>
+        <translation>Изображение, содержащее синий канал</translation>
+    </message>
+    <message>
+        <location filename="v_calculator_dialog_base.ui" line="20"/>
+        <source>Vegetation сalculator</source>
+        <translation>Вегетационный калькулятор</translation>
+    </message>
+</context>
+</TS>
